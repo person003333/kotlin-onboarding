48a3b7f8144e08441f85a646d4bb303891b7c35b
박지민 <pjm5489@naver.com>
2024-09-16 04:59:36 +0900

    feat: 배열 아이템 삭제 기능 구현

--- src/main/kotlin/onboarding/Problem2.kt
+++ src/main/kotlin/onboarding/Problem2.kt
@@ -2,10 +2,17 @@ package onboarding
 
 fun solution2(cryptogram: String): String {
     val cryptogramList = mutableListOf<Any>()
+    var i = 0
     for (char in cryptogram) {
         cryptogramList.add(char.toString())
     }
-
+    while (i < cryptogramList.size - 1) {
+        if (cryptogramList[i] == cryptogramList[i + 1]) {
+            cryptogramList.removeAt(i)
+            cryptogramList.removeAt(i)
+            i -= 2
+        }
+       i++
+    }
     return cryptogramList.toString()
 }
-
