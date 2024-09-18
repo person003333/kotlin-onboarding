edf5c6ca8e6c85985f66f6b2743d75c5195e624c
박지민 <pjm5489@naver.com>
2024-09-18 19:37:29 +0900

    feat: word변환 기능 구현

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,5 +1,27 @@
 package onboarding
 
 fun solution4(word: String): String {
-    TODO("프로그램 구현")
+    val alphabetList = listOf(
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    )
+    val wordList = mutableListOf<Char>()
+    for (char in word) {
+        wordList.add(char)
+    }
+
+    for (j in alphabetList.indices) {
+        for (i in 0 until wordList.size) {
+            if (wordList[i] == alphabetList[j]) {
+                wordList[i] = alphabetList[25 - j]
+            } else if (wordList[i] == alphabetList[j].lowercaseChar()) {
+                wordList[i] = alphabetList[25 - j].lowercaseChar()
+            }
+        }
+    }
+   
+}
+
+fun main() {
+    println(solution4("I love you"))
 }
