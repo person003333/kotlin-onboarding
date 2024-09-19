97445af7d69d3896ec59aa1fa970fc7f9083e5b8
박지민 <pjm5489@naver.com>
2024-09-19 10:35:46 +0900

    refactor: 로직 변경

--- src/main/kotlin/onboarding/Problem4.kt
+++ src/main/kotlin/onboarding/Problem4.kt
@@ -1,30 +1,28 @@
 package onboarding
 
 fun solution4(word: String): String {
-    val alphabetList = listOf(
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-    )
+    val alphabetList = ('A'..'Z').toList()
     val wordList = mutableListOf<Char>()
+    val result = StringBuilder()
     for (char in word) {
         wordList.add(char)
     }
     for (i in wordList.indices) {
-    for (j in alphabetList.indices) {
-            if (wordList[i] == alphabetList[j]) {
-                wordList[i] = alphabetList[25 - j]
-                break
-            } else if (wordList[i] == alphabetList[j].lowercaseChar()) {
-                wordList[i] = alphabetList[25 - j].lowercaseChar()
-                break
+        for (j in alphabetList.indices) {
+            when {
+                wordList[i] == alphabetList[j] -> {
+                    wordList[i] = alphabetList[25 - j]
+                    break
+                }
+
+                wordList[i] == alphabetList[j].lowercaseChar() -> {
+                    wordList[i] = alphabetList[25 - j].lowercaseChar()
+                    break
+                }
             }
         }
+        result.append(wordList[i])
     }
-    val stringBuilder = StringBuilder()
-    var result = ""
-    for (i in wordList.indices) {
-        result = stringBuilder.append(wordList[i]).toString()
-    }
-    return result
+    return result.toString()
 }
 
